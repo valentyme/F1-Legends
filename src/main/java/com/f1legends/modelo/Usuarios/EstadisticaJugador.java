e881d73b25c181f28196cd905fed72e9be6d64ba
Facundo Castillo Ivanauskas <facundocastilloiva@gmail.com>
2026-06-20 22:09:27 -0300

    Explicacion bdd y eliminacion clases fantasmas

--- src/main/java/com/f1legends/modelo/Usuarios/EstadisticaJugador.java
+++
@@ -1,8 +0,0 @@
-package com.f1legends.modelo.Usuarios;
-
-public class EstadisticaJugador {
-    private int posicionesGanadas;
-    private int posicionesPerdidas;
-    private int entradasBoxes;
-    private int vueltasCompletadas;
-}
